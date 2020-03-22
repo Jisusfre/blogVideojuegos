e2d58fea6c2dab3caf61d1a7e23114153cc94ee1
Jisusfre <Jisusfre@gmail.com>
2020-03-21 21:41:35 -0600

    Un poco del home

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,26 @@
 
 
 const Home =() =>{
-    const view=``;
+    const view=`
+      <div class="container mt-5 ">
+        <div id="carouselExampleSlidesOnly" class="carousel slide " data-ride="carousel">
+        <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="../src/assets/images/controles.jpg" class="d-block w-80 rounded " alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="../src/assets/images/lolsito.png" class="d-block w-80 rounded" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="../src/assets/images/gears.jpeg" class="d-block w-80 rounded" alt="...">
+        </div>
+        <div class="carousel-item">
+        <img src="../src/assets/images/halo1.jpeg" class="d-block w-80 rounded" alt="...">
+        </div>
+      </div>
+      </div>
+    </div>
+    `;
     return view;
 
 }
